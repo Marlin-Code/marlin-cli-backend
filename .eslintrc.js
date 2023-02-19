81e7c4df318fa44c8e199cd661dd9cd3314ccbe0
George Cooper <george@marlincode.com>
2023-02-19 10:30:27 -0500

    husky eslint prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,12 +4,16 @@ module.exports = {
     es2021: true,
   },
   extends: 'airbnb-base',
-  overrides: [
-  ],
+  overrides: [],
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
+  plugins: ['react', 'prettier'],
   rules: {
+    semi: 0,
+    'comma-dangle': 0,
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
   },
 };
